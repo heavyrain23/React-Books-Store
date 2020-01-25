96a106e77a69e0e963f6d501cd492d3b32a40942
Alex <failemotions95@gmail.com>
2020-01-25 14:26:47 +0200

    Updates

---
+++ precache-manifest.5c72322982b6e5a001cb7fd1c72f9008.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b26ef3f77132b141e21e727e55acad",
+    "url": "/React-Books-Store/index.html"
+  },
+  {
+    "revision": "8ecf43853c4eb6cbc55d",
+    "url": "/React-Books-Store/static/css/2.44586863.chunk.css"
+  },
+  {
+    "revision": "8ecf43853c4eb6cbc55d",
+    "url": "/React-Books-Store/static/js/2.d039daa6.chunk.js"
+  },
+  {
+    "revision": "c32415b0993c41a4c12b137ce9c5d6cf",
+    "url": "/React-Books-Store/static/js/2.d039daa6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "061a220eee6c334ba227",
+    "url": "/React-Books-Store/static/js/main.9a1db1cf.chunk.js"
+  },
+  {
+    "revision": "2d67da4fe5156ce48133",
+    "url": "/React-Books-Store/static/js/runtime-main.43e4321f.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/React-Books-Store/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/React-Books-Store/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/React-Books-Store/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/React-Books-Store/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/React-Books-Store/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/React-Books-Store/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/React-Books-Store/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/React-Books-Store/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/React-Books-Store/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/React-Books-Store/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/React-Books-Store/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/React-Books-Store/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/React-Books-Store/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/React-Books-Store/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/React-Books-Store/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/React-Books-Store/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
